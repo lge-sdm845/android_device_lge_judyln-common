5a628f579c278c5076871b3b087cce0b13256c8b
Han Sol Jin <hansol@hansol.ca>
2025-01-04 00:41:10 -0800

    judyln-common: switch to py extract-utils
    Change-Id: I40b9f58615c821adf81060caf027cf26aa723d54

---
+++ extract-files.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixups,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    "vendor/lge/sdm845-common",
+    "vendor/qcom/opensource/display",
+]
+
+
+blob_fixups: blob_fixups_user_type = {
+    (
+        'vendor/lib/liblgsnpeawb.so',
+        'vendor/lib/libSNPE.so'
+    ): blob_fixup()
+        .replace_needed('libstdc++.so', 'libstdc++_vendor.so')
+}  # fmt: skip
+
+
+module = ExtractUtilsModule(
+    'judyln-common',
+    'lge',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
